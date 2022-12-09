85edf72b6b33b2631e788c8e04024fa246634d81
viniciusrodriguesb <viniciusrbastos@hotmail.com>
2022-12-09 10:06:45 -0300

    Insercao do CarteiraService

--- API_MyWallet/app/src/main/java/service/CarteiraService.java
+++ API_MyWallet/app/src/main/java/service/CarteiraService.java
@@ -1,7 +1,30 @@
 package service;
 
+import java.util.List;
+
+import dao.CarteiraDAO;
+import model.Carteira;
+
 public class CarteiraService {
-	CarteiraDAO crt = new CarteiraDAO();
-	
-	
+	CarteiraDAO carteira = new CarteiraDAO();
+
+	public List<Carteira> listarCarteiras(int idCarteira) {
+		return carteira.listarCarteira(idCarteira);
+	}
+
+	public Carteira procurarCarteira(int id) {
+		return carteira.procurarCarteira(id);
+	}
+
+	public boolean cadastrarCarteira(Carteira cdt) {
+		return carteira.cadastrarCarteira(cdt);
+	}
+
+	public boolean deletarCarteira(int idCarteira) {
+		return carteira.deletarCarteira(idCarteira);
+	}
+
+	public boolean modificarCarteira(Carteira cdt) {
+		return carteira.modificarCarteira(cdt);
+	}
 }
