c89fc9ba4e86f7a02d354347525017b022cd8446
matheus victor <matheusvictorgdr15@gmail.com>
2022-12-14 16:49:30 -0300

    Implementação do UsuarioService completa

---
+++ API_MyWallet/app/src/main/java/service/UsuarioService.java
@@ -0,0 +1,35 @@
+package service;
+
+import dao.UsuarioDAO;
+import model.Usuario;
+
+public class UsuarioService {
+	
+	UsuarioDAO usuario = new UsuarioDAO();
+	
+	//Procura um usuário a partir de um id
+	public Usuario procurarUsuario(int idUsuario){
+		
+		return usuario.procurarUsuario(idUsuario);
+
+	}
+	
+	//Cadastra um usuário no banco de dados
+	public boolean cadastrarUsuario(Usuario usu){
+		
+		return usuario.cadastrarUsuario(usu);
+	}
+	
+	//Deleta um usuário do banco de dados
+	public boolean deletarUsuario(int idUsuario){
+
+		return usuario.deletarUsuario(idUsuario);
+
+	}
+	
+	//Modifica um usuário do banco de dados
+	public boolean  modificarUsuario(Usuario usu){
+
+		return usuario.modificarUsuario(usu);
+	}
+}
