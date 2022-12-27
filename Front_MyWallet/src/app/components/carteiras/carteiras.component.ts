0fc5e1a77119166e01e82d670af5dd200e94797a
matheus victor <matheusvictorgdr15@gmail.com>
2022-12-26 21:55:58 -0300

    modal da adição de carteiras

--- Front_MyWallet/src/app/components/carteiras/carteiras.component.ts
+++ Front_MyWallet/src/app/components/carteiras/carteiras.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'mw-carteiras',
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarteirasComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('myBtn') btn: ElementRef | undefined;
+  @ViewChild('close') close: ElementRef | undefined;
+  @ViewChild('myModal') modal: ElementRef | undefined;
+  @ViewChild('modalContent') modalContent: ElementRef | undefined;
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
   }
 
+  mostrar(){
+    const btn = this.btn?.nativeElement;
+    const close = this.close?.nativeElement;
+    const modal = this.modal?.nativeElement;
+    const modalContent = this.modalContent?.nativeElement;
+
+   // this.renderer.setStyle(close, 'display', "none");
+    this.renderer.setStyle(modal, 'display', "block");
+    this.renderer.setStyle(modalContent, 'top', "0");
+    //this.renderer.setStyle(modalContent, 'display', "none");
+  }
+
+  fechar(){
+    const modal = this.modal?.nativeElement;
+    this.renderer.setStyle(modal, 'display', "none");
+  }
+
+  fecharModal(){
+    const modal = this.modal?.nativeElement;
+    this.renderer.setStyle(modal, 'display', "none");
+  }
 }
