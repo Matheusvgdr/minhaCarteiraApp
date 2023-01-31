6d1cfca39135d41609e44c51bc0643864c9ba6c0
viniciusrodriguesb <viniciusrbastos@hotmail.com>
2023-01-31 07:51:13 -0300

    Implementando realizar transacao

--- Mobile_MyWallet/src/app/components/transacao/transacao.page.ts
+++ Mobile_MyWallet/src/app/components/transacao/transacao.page.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { Banco } from 'src/app/models/banco.model';
+import { Carteira } from 'src/app/models/carteira.model';
+import { Movi } from 'src/app/models/movimentacao.model';
 import { TT } from 'src/app/models/tipoTransacao.mode';
+import { Usuario } from 'src/app/models/usuario.model';
 import { BancoService } from 'src/app/service/banco.service';
+import { CarteiraService } from 'src/app/service/carteira.service';
+import { MovimentacaoService } from 'src/app/service/movimentacao.service';
 import { TipoTransacaoService } from 'src/app/service/tipo-transacao.service';
 
 @Component({
@@ -12,17 +17,69 @@ import { TipoTransacaoService } from 'src/app/service/tipo-transacao.service';
 })
 export class TransacaoPage implements OnInit {
   tiposT!: TT[];
-  bancos!: Banco[];
+  bancos!: Banco[]; 
+  carteiras!: Carteira[];
+  
+  dinheiro!: number;
+  data!: Date;
 
-  constructor(private servico: TipoTransacaoService, private bancoServ: BancoService, private navControl: NavController) { }
+  usuario: Usuario = JSON.parse(sessionStorage.getItem("usuario") || "") as Usuario;
+
+  banco: Banco = {
+    id: 0,
+    banco: "",
+    id_usuario: this.usuario
+  }
+
+  carteira: Carteira ={
+    id: 0,
+    nomeCarteira: "",
+    dinheiro: 0,
+    id_usuario: this.usuario,
+    id_banco: this.banco
+  }
+
+  tipo: TT = {
+    id: 0,
+    descricao: ""
+  }
+  
+  movimentacao: Movi = {
+    dataMovimentacao: "",
+    dinheiro: 0,
+    id_banco: this.banco,
+    id_usuario: this.usuario,
+    id_tipo: this.tipo
+  }
+
+teste(){
+  console.log(this.movimentacao);
+}
+
+
+  constructor(private servico: TipoTransacaoService, private bancoServ: BancoService, private carteiraServ: CarteiraService, private movimentacaoServ: MovimentacaoService, private navControl: NavController) { }
+
+ 
+  ngOnInit(): void {
+    this.getTipoTransacao();
+    this.getListarBanco();
+    this.getListarCarteiras(this.usuario.id || 0);
+  }
+
+  realizarTransacao(){
+    this.postRealizarTransacao();
+  }
 
   voltar(){
     this.navControl.navigateForward("home");
   }
 
-  ngOnInit(): void {
-    this.getTipoTransacao();
-    this.getListarBanco();
+  private getListarCarteiras(idUsuario: number){
+    this.carteiraServ.getListarCarteiras(idUsuario).subscribe({
+      next: (response) => {
+        this.carteiras = response;
+      }
+    })
   }
 
   private getTipoTransacao(){
@@ -42,4 +99,13 @@ export class TransacaoPage implements OnInit {
       }
     })
   }
+
+    private postRealizarTransacao(){
+      this.movimentacaoServ.postRealizarTransacao(this.movimentacao).subscribe({
+      next: (response) => {
+        this.movimentacao = response;
+        console.log(response);
+      }
+    })
+  } 
 }
